acf8b8e9322f07310889649c9948b9b5acbb712c
nextor <nextor@gmail.com>
2025-05-04 13:13:05 -0700

    First commits

--- src/app/pages/list-products/list-products.component.ts
+++ src/app/pages/list-products/list-products.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-list-products',
-  imports: [],
+	imports: [CommonModule],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
-export class ListProductsComponent {
+export class ListProductsComponent implements OnInit
+{
+    item_info_list:any[] = [];
 
+		ngOnInit(): void
+		{
+
+			fetch('https://uniformesprofesionales.integranet.xyz/api/item_info.php?limit=20')
+			.then((response)=>
+			{
+				return response.json();
+			})
+			.then((response)=>
+			{
+				this.item_info_list = response.data;
+			});
 }
+}
+
