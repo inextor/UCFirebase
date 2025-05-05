dcef708017fb185b6c9d5430cd27f206e3d01a89
nextor <nextor@gmail.com>
2025-05-05 02:51:37 +0000

    Add login page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListProductsComponent } from './pages/list-products/list-products.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ListProductsComponent,
+  },
+  {
+    path: '/login',
+    component: LoginComponent,
   },
 ];
