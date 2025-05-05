9bf67e611c1d45714ea7758f5eed5273d0de34d8
nextor <nextor@gmail.com>
2025-05-05 03:20:20 +0000

    feat: add session control to page-header

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './page-header.component.html',
-  styleUrl: './page-header.component.css'
+  styleUrl: './page-header.component.css',
 })
-export class PageHeaderComponent {
+export class PageHeaderComponent implements OnInit {
+  isLoggedIn = false;
 
+  ngOnInit(): void {
+    this.checkSession();
+  }
+
+  checkSession() {
+    const session = localStorage.getItem('session');
+    this.isLoggedIn = !!session; // Check if session exists
+  }
+
+  logout() {
+    localStorage.removeItem('session');
+    this.isLoggedIn = false;
+  }
 }
+
