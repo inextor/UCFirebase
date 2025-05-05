1e4e26a8ca7c6b78b80bc9142a4511de655ca1de
nextor <nextor@gmail.com>
2025-05-05 04:54:18 +0000

    feat: add list-user, save-user components and rest service

---
+++ src/app/pages/save-user/save-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save-user',
+  imports: [],
+  templateUrl: './save-user.component.html',
+  styleUrl: './save-user.component.css'
+})
+export class SaveUserComponent {
+
+}
