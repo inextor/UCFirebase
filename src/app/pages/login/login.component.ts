48e71c433f93e771e621ec9f4e932c3f7bf1fae6
nextor <nextor@gmail.com>
2025-05-05 03:06:01 +0000

    Fix: unsubscribe http subscription

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
+  username = '';
+  password = '';
+  http = inject(HttpClient);
+  private destroy$ = new Subject<void>();
 
+  login() {
+    const payload = {
+      username: this.username,
+      password: this.password,
+    };
+    
+    this.http
+      .post('https://trikitrakes.integranet.xyz/api/login.php', payload)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          try {
+            const responseJson = JSON.parse(JSON.stringify(response));
+            const session = responseJson.session;
+            localStorage.setItem('session', session);
+            console.log('Session stored:', session);
+
+          } catch (error) {
+            console.error('Error parsing JSON response:', error);
+          }
+        },
+        error: (error) => {
+          console.error('Login error:', error);
+        },
+      });
+  }
+  
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
